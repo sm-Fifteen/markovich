a5746b70aebb43d642a372920d46e1929c0853b7
sm-Fifteen <516999+sm-Fifteen@users.noreply.github.com>
2018-10-03 21:43:05 -0400

    Made record_and_generate the main entry point, so the other two only do simple SQL

--- test-sqlite.js
+++ test-sqlite.js
@@ -15,50 +15,62 @@ function MarkovichSQLiteBackend(namespace_id) {
 	`);
 
 	this.db.run("CREATE INDEX IF NOT EXISTS chain_link1_idx ON chain (link1);");
-	
-	this.generate_statement = this.db.prepare(generate_sql);
 }
 
-const generate_sql = `
-WITH RECURSIVE markov(last_word, current_word, random_const) AS (
-	-- Can't use subquery CTEs for random constants, since, in SQLite,
-	-- they end up being evaluated once for the entire markov chain
-	VALUES(?, ?, ABS(random()) / CAST(0x7FFFFFFFFFFFFFFF AS real))
-UNION ALL
-	-- Can't do correlated subqueries/lateral joins in sqlite, but...
-	-- ...I guess a one-row, one-col subquery inside a SELECT works! ¯\_(ツ)_/¯
-	SELECT markov.current_word, (
-		-- Weighted word generation
-		-- Loosely based on https://stackoverflow.com/a/13040717
-		SELECT link2 FROM (
-			SELECT link1, link2, n,
-			SUM(n) OVER (PARTITION BY link1 ROWS UNBOUNDED PRECEDING) AS rank,
-			SUM(n) OVER (PARTITION BY link1) * markov.random_const AS roll
-			FROM chain
-			WHERE link1 = markov.current_word
-		) t WHERE roll <= rank LIMIT 1
-	) AS next_word,
-	ABS(random()) / CAST(0x7FFFFFFFFFFFFFFF AS real) AS random_const
+MarkovichSQLiteBackend.prototype.record_and_generate = function(sentence, max_length, split_pattern) {
+	if (!split_pattern) split_pattern  = "[, ]";
+	if (!sentence) return Promise.resolve([]);
 	
-	FROM markov
-	WHERE current_word <> ' '
-) SELECT * FROM markov LIMIT 25;
-`;
-
-MarkovichSQLiteBackend.prototype.generate_from_sentence = function(sentence, split_pattern) {
 	let words = sentence.split(new RegExp(split_pattern));
-	let first_idx = Math.floor(Math.random()*(words.length - 1));
-	let seed = [words[first_idx], words[first_idx+1]];
 	
-	return new Promise((resolve, reject) => this.generate_statement.all(seed, function(err, rows) {
+	return this.record_words(words).then((function() {
+		// FIXME : I hate having to do this, but SQLite can't split strings itsef
+		if (max_length < 1 || words.length < 2) return [];
+		
+		let seed1_idx = Math.floor(Math.random()*(words.length - 1));
+		
+		return this.generate_from_pair(words[seed1_idx], words[seed1_idx+1], max_length);
+	}).bind(this));
+}
+
+MarkovichSQLiteBackend.prototype.generate_from_pair = function(seed1, seed2, max_length) {
+	if (!this.generate_statement) this.generate_statement = this.db.prepare(`
+		WITH RECURSIVE markov(last_word, current_word, random_const) AS (
+			-- Can't use subquery CTEs for random constants, since, in SQLite,
+			-- they end up being evaluated once for the entire markov chain
+			VALUES(?, ?, ABS(random()) / CAST(0x7FFFFFFFFFFFFFFF AS real))
+		UNION ALL
+			-- Can't do correlated subqueries/lateral joins in sqlite, but...
+			-- ...I guess a one-row, one-col subquery inside a SELECT works! ¯\_(ツ)_/¯
+			SELECT markov.current_word, (
+				-- Weighted word generation
+				-- Loosely based on https://stackoverflow.com/a/13040717
+				SELECT link2 FROM (
+					SELECT link1, link2, n,
+					SUM(n) OVER (PARTITION BY link1 ROWS UNBOUNDED PRECEDING) AS rank,
+					SUM(n) OVER (PARTITION BY link1) * markov.random_const AS roll
+					FROM chain
+					WHERE link1 = markov.current_word
+				) t WHERE roll <= rank LIMIT 1
+			) AS next_word,
+			ABS(random()) / CAST(0x7FFFFFFFFFFFFFFF AS real) AS random_const
+			
+			FROM markov
+			WHERE current_word <> ' '
+		) SELECT last_word FROM markov LIMIT ?;
+	`);
+	
+	if (!seed2) seed2 = ' ';
+	if (max_length < 1) return Promise.resolve(null);
+	
+	return new Promise((resolve, reject) => this.generate_statement.all([seed1, seed2, max_length], function(err, rows) {
 		if (err) reject(err);
-		resolve(rows);
+		resolve(rows.map((row) => row.last_word));
 	}));
 }
 
-MarkovichSQLiteBackend.prototype.record_sentence = function(sentence, split_pattern) {
-	let words = sentence.split(new RegExp(split_pattern));
-	let sql_param_list = Array.from(words, () => "(?)").join(', ')
+MarkovichSQLiteBackend.prototype.record_words = function(words) {
+	let sql_param_list = Array.from(words, () => "(?)").join(', ');
 	
 	let record_sql = `
 		WITH
@@ -75,9 +87,9 @@ MarkovichSQLiteBackend.prototype.record_sentence = function(sentence, split_patt
 	}));
 }
 
+/*
 let markov = new MarkovichSQLiteBackend('test_db2');
-markov.record_sentence("the world is DOOMED!", "[, ]")
-.then(() => markov.generate_from_sentence("Hello world", "[, ]"))
+markov.record_and_generate("One", 25)
 .then(console.log)
 .catch(console.error);
-
+*/
