f4566fd93d801d20a49013f9227740ea38d6c3ff
sm-Fifteen <516999+sm-Fifteen@users.noreply.github.com>
2019-02-09 20:41:16 -0500

    Require config file to be passed as command-line argument

--- markovich/__main__.py
+++ markovich/__main__.py
@@ -1,3 +1,4 @@
+import sys, os
 import json
 from typing import List, Callable
 from asyncio import Future, get_event_loop
@@ -6,26 +7,33 @@ from .markovich_discord import run_markovich_discord
 from .markovich_irc import run_markovich_irc
 from .markovich_cli import run_markovich_cli
 
-config_path = "./config.json"
+def run_with_config(config):
+	cleanup_functions = [] #type: List[Callable[[], Future]]
+	aio_loop = get_event_loop()
 
-with open(config_path, 'r') as config_file:
-	config = json.load(config_file)
+	try:
+		if 'irc' in config:
+			cleanup_fn = run_markovich_irc(config['irc'], eventloop=aio_loop)
+			#cleanup_functions.append(cleanup_fn)
 
-cleanup_functions = [] #type: List[Callable[[], Future]]
-aio_loop = get_event_loop()
+		if 'discord' in config:
+			cleanup_fn = run_markovich_discord(config['discord'], eventloop=aio_loop)
+			cleanup_functions.append(cleanup_fn)
 
-try:
-	if 'irc' in config:
-		cleanup_fn = run_markovich_irc(config['irc'], eventloop=aio_loop)
-		#cleanup_functions.append(cleanup_fn)
+		aio_loop.run_forever()
+	except KeyboardInterrupt:
+		print("Shutting down")
+	finally:
+		for cleanup in cleanup_functions:
+			aio_loop.run_until_complete(cleanup())
 
-	if 'discord' in config:
-		cleanup_fn = run_markovich_discord(config['discord'], eventloop=aio_loop)
-		cleanup_functions.append(cleanup_fn)
+if len(sys.argv) > 1:
+	config_path = sys.argv[1]
 
-	aio_loop.run_forever()
-except KeyboardInterrupt:
-	print("Shutting down")
-finally:
-	for cleanup in cleanup_functions:
-		aio_loop.run_until_complete(cleanup())
+	with open(config_path, 'r') as config_file:
+		config = json.load(config_file)
+	
+	run_with_config(config)
+else:
+	print("Called with no configuration file, launching in test mode")
+	run_markovich_cli()
